711a49f1e375a4753972fe91d698692d73d77bb0
shijiaa <shijihcl@gmail.com>
2025-02-12 23:21:24 +0530

    c

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,25 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "localhost",
-    port: 4000,
-  },
-  build: {
-   
-    chunkSizeWarningLimit: 500,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("react")) return "vendor-react"; // React-related chunks
-            if (id.includes("axios")) return "vendor-axios"; // Axios chunk
-            return "vendor"; // Other dependencies
-          }
-        },
-      },
-    },
-  },
-});
+  server:{
+    host: 'localhost',
+    port: 4000
+  }
+})
+
+
