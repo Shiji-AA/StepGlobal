d9d81110f45c4875efda4060b12a404c9e4d573a
shijiaa <shijihcl@gmail.com>
2025-02-18 11:37:09 +0530

    changes added in backend

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -6,6 +6,7 @@ import { connectDB } from './config/db.js';
 import recruiterRouter from './Routes/RecruiterRoutes/RecruiterRoutes.js';
 import adminRouter from './Routes/AdminRoutes/AdminRoutes.js';
 
+
 dotenv.config()
 connectDB()
 
@@ -13,10 +14,27 @@ const app= express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(cors());
+
+const corsOptions = {
+    origin: ['http://localhost:4000', 'https://stepglobal.in', 'https://www.stepglobal.in', 'http://localhost:3000'],
+    methods: "GET, PUT, POST, PATCH, DELETE",
+    allowedHeaders: ["Content-Type", "Authorization"]
+  };
+  app.use(cors(corsOptions));
+
+
 app.use('/api/users',UserRoutes)
 app.use('/api/recruiter',recruiterRouter)
 app.use('/api/admin',adminRouter);
+
+// Serving static files
+app.use(express.static(join(__dirname, '../../Frontend/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(join(__dirname, "../../Frontend/dist/index.html"));
+  });
+  
+
 app.get('/',(req,res)=>res.send("server is ready"))
 const port= process.env.PORT||3000
 
