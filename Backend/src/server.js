f643e4ae55f33adaf212b643351f87bac793730e
shijiaa <shijihcl@gmail.com>
2025-02-18 11:41:50 +0530

    ch

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,42 +1,40 @@
-import express from  'express';
+import express from 'express';
 import cors from "cors";
 import dotenv from 'dotenv';
+import { join } from 'path';  // Correct import
 import UserRoutes from './Routes/UserRoutes/UserRoutes.js'
 import { connectDB } from './config/db.js';
 import recruiterRouter from './Routes/RecruiterRoutes/RecruiterRoutes.js';
 import adminRouter from './Routes/AdminRoutes/AdminRoutes.js';
 
+dotenv.config();
+connectDB();
 
-dotenv.config()
-connectDB()
+const app = express();
 
-const app= express()
-
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-    origin: ['http://localhost:4000', 'https://stepglobal.in', 'https://www.stepglobal.in', 'http://localhost:3000'],
-    methods: "GET, PUT, POST, PATCH, DELETE",
-    allowedHeaders: ["Content-Type", "Authorization"]
-  };
-  app.use(cors(corsOptions));
+  origin: ['http://localhost:4000', 'https://stepglobal.in', 'https://www.stepglobal.in', 'http://localhost:3000'],
+  methods: "GET, PUT, POST, PATCH, DELETE",
+  allowedHeaders: ["Content-Type", "Authorization"]
+};
+app.use(cors(corsOptions));
 
-
-app.use('/api/users',UserRoutes)
-app.use('/api/recruiter',recruiterRouter)
-app.use('/api/admin',adminRouter);
+app.use('/api/users', UserRoutes);
+app.use('/api/recruiter', recruiterRouter);
+app.use('/api/admin', adminRouter);
 
 // Serving static files
 app.use(express.static(join(__dirname, '../../Frontend/dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(join(__dirname, "../../Frontend/dist/index.html"));
-  });
-  
+  res.sendFile(join(__dirname, "../../Frontend/dist/index.html"));
+});
 
-app.get('/',(req,res)=>res.send("server is ready"))
-const port= process.env.PORT||3000
+app.get('/', (req, res) => res.send("server is ready"));
 
-app.listen(port,()=>
-console.log("server started"))
+const port = process.env.PORT || 3000;
+app.listen(port, () =>
+  console.log("server started"));
