aad793e5576109e4b254b5671983ec8d770db477
shijiaa <shijihcl@gmail.com>
2025-02-28 14:57:20 +0530

    c

--- Frontend/src/Components/Users/Architecture/ArchitectureHero/ArchitectureHero1.jsx
+++ Frontend/src/Components/Users/Architecture/ArchitectureHero/ArchitectureHero1.jsx
@@ -4,7 +4,7 @@ import img2 from "../../../../assets/architect/2.jpg"
 import img3 from "../../../../assets/architect/3.jpg"
 import img4 from "../../../../assets/architect/1.jpg"
 import img5 from "../../../../assets/architect/2.jpg"
-import img6 from "../../../../assets/architect/3.jpg"
+// import img6 from "../../../../assets/architect/3.jpg"
 
 function ArchitectureHero1() {
     return (
@@ -12,10 +12,10 @@ function ArchitectureHero1() {
             {/* ONE */}
             <section className="h-screen overflow-hidden bg-cover bg-center" style={{ backgroundImage: `url(${bg2})` }}>
                 <div className="flex flex-col lg:flex-row items-center justify-between h-full bg-black opacity-85">
-
+                    
                     {/* Left Side */}
-                    <div className="p-6 sm:p-10 lg:p-20 flex-1 text-center lg:text-left">
-                        <p className="font-custom text-3xl sm:text-4xl md:text-5xl lg:text-[5.5rem] leading-none mb-6 sm:mb-8 lg:mb-10 text-white">
+                    <div className="p-6 lg:p-20 flex-1 text-center lg:text-left">
+                        <p className="font-custom text-[3.5rem] lg:text-[5.5rem] leading-none mb-6 lg:mb-10 text-white">
                             The fastest <br /> on planet
                         </p>
                         <div className="flex flex-col sm:flex-row gap-4 justify-center lg:justify-start">
@@ -23,17 +23,15 @@ function ArchitectureHero1() {
                             <button className="font-custom btn border border-gray-600 text-white w-full sm:w-auto">Get Consultation</button>
                         </div>
                     </div>
-
+                    
                     {/* Right Side (Images) */}
-                    <div className="flex-1 flex flex-wrap justify-center gap-6 sm:gap-4 lg:gap-6">
-
-                        <img className="img max-w-[40%] sm:max-w-[45%] lg:max-w-[30%]" src={img1} alt="img1" />
-                        <img className="img max-w-[40%] sm:max-w-[45%] lg:max-w-[30%]" src={img2} alt="img2" />
-                        <img className="img max-w-[40%] sm:max-w-[45%] lg:max-w-[30%]" src={img3} alt="img3" />
-                        <img className="img max-w-[40%] sm:max-w-[45%] lg:max-w-[30%]" src={img4} alt="img4" />
-                        <img className="img max-w-[40%] sm:max-w-[45%] lg:max-w-[30%]" src={img5} alt="img5" />
-                        <img className="img max-w-[40%] sm:max-w-[45%] lg:max-w-[30%]" src={img6} alt="img6" />
-
+                    <div className="flex-1 flex flex-wrap gap-6 justify-center lg:justify-end">
+                        <img className="img max-w-[45%] lg:max-w-full" src={img1} alt="img1" />
+                        <img className="img max-w-[45%] lg:max-w-full" src={img2} alt="img2" />
+                        <img className="img max-w-[45%] lg:max-w-full" src={img3} alt="img3" />
+                        <img className="img max-w-[45%] lg:max-w-full" src={img4} alt="img4" />
+                        <img className="img max-w-[45%] lg:max-w-full" src={img5} alt="img5" />
+                        {/* <img className="img max-w-[45%] lg:max-w-full" src={img6} alt="img6" /> */}
                     </div>
                 </div>
             </section>
--- Frontend/src/Components/Users/Architecture/ArchitectureHero/ArchitectureHero2.jsx
+++ Frontend/src/Components/Users/Architecture/ArchitectureHero/ArchitectureHero2.jsx
@@ -2,14 +2,14 @@ function ArchitectureHero2() {
     return (
         <>
             <section className="py-10 sm:py-14 md:py-20 px-6 sm:px-10 lg:px-40">
-                <div className="py-10 sm:py-14 md:py-20 grid grid-cols-1 md:grid-cols-2 gap-10 border-b border-gray-300">
+                <div className="py-10 sm:py-14 md:py-20 flex flex-col md:flex-col gap-5 border-b border-gray-300">
                     {/* Heading */}
                     <h1 className="font-custom text-gray-800 text-4xl sm:text-5xl md:text-6xl leading-normal font-normal text-left">
-    The property that <span className="hidden md:inline"> <br /> </span> suits you perfectly
-</h1>
+                        The property that <span className="hidden md:inline"> <br /> </span> suits you perfectly
+                    </h1>
 
                     {/* Paragraph */}
-                    <p className="font-custom text-gray-700 text-xl sm:text-2xl md:text-3xl leading-normal text-left">
+                    <p className="font-custom text-gray-700 text-xl sm:text-2xl md:text-3xl leading-normal text-left pt-4">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, quis eveniet placeat magni ipsum dolore
                         git delectus velit aliquam saepe facere voluptatibus? Eligendi nulla earum
                     </p>
